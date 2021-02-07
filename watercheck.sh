3e2b99faad7957d7b2c442bf38731e1fd1c3ac17
logon84 <logon84@users.noreply.github.com>
2021-02-08 00:23:03 +0100

    Update watercheck.sh

--- watercheck.sh
+++ watercheck.sh
@@ -2,6 +2,7 @@
 #Designed for Openwrt. Cron this every 15min
 
 my_smartphone_mac = "01:23:45:67:89:ab"
+mqtt_broker = "192.168.1.1"
 mqtt_user = "mqtt_user_here"
 mqtt_password = "mqtt_password_here"
 
@@ -18,7 +19,7 @@ amIpresent=$(arp -a | grep $my_smartphone_mac | tr -d ' ')
 
 if [ -z $amIpresent ]; then
 	#I'm out of home
-	waterflow_status=$(/opt/usr/bin/mosquitto_sub -h 192.168.1.1 -u $mqtt_user -P mqtt_password -t waterflow -q 2 -C 1 -W 5 | jq -r ".moved_last_half_hour")
+	waterflow_status=$(/opt/usr/bin/mosquitto_sub -h $mqtt_broker -u $mqtt_user -P mqtt_password -t waterflow -q 2 -C 1 -W 5 | jq -r ".moved_last_half_hour")
 	if [ ! -z $waterflow_status ]; then
 		if [ ! -e /opt/usr/sbin/waterflow_sensor.strike ]; then
 			#first water check after leaving home
