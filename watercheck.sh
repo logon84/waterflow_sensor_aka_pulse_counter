df12c8bf35458aae8c03044a0847cd0465310165
logon84 <logon84@users.noreply.github.com>
2021-02-08 21:23:41 +0100

    Update watercheck.sh

--- watercheck.sh
+++ watercheck.sh
@@ -19,52 +19,27 @@ amIpresent=$(arp -a | grep $my_smartphone_mac | tr -d ' ')
 
 if [ -z $amIpresent ]; then
 	#I'm out of home
-	waterflow_status=$(/opt/usr/bin/mosquitto_sub -h $mqtt_broker -u $mqtt_user -P $mqtt_password -t waterflow -q 2 -C 1 -W 5 | jq -r ".moved_last_half_hour")
+	waterflow_status=$(/opt/usr/bin/mosquitto_sub -h 192.168.1.1 -u ea8500 -P ea8500 -t waterflow -q 2 -C 1 -W 5)
+	edges_count=$(echo $waterflow_status | jq -r ".edges")
 	if [ ! -z $waterflow_status ]; then
-		if [ ! -e /opt/usr/sbin/waterflow_sensor.strike ]; then
+		if [ ! -e /opt/usr/sbin/waterflow_sensor.edges ]; then
 			#first water check after leaving home
-			if [ $waterflow_status -eq 1 ]; then
-				echo "1" > /opt/usr/sbin/waterflow_sensor.strike
-			else
-				echo "0" > /opt/usr/sbin/waterflow_sensor.strike
-			fi
-			echo $waterflow_status > /opt/usr/sbin/waterflow_sensor.last
+			echo $edges_count > /opt/usr/sbin/waterflow_sensor.edges
 		else
 			#subsequent checks after leaving home
-			waterflow_sensor_last=$(awk 'NR==1 {print; exit}' /opt/usr/sbin/waterflow_sensor.last)
-			if [ $waterflow_sensor_last -eq 0 ] && [ $waterflow_status -eq 0 ]; then
-				#All ok at home, reset strikes
-				echo "0" > /opt/usr/sbin/waterflow_sensor.strike
-			elif [ $waterflow_sensor_last -eq 0 ] && [ $waterflow_status -eq 1 ]; then
-				#Waterflow detected. Alarm!
+			edges_count_prev=$(awk 'NR==1 {print; exit}' /opt/usr/sbin/waterflow_sensor.edges)
+			echo $edges_count > /opt/usr/sbin/waterflow_sensor.edges
+			if [ $edges_count -ne $edges_count_prev ]; then
+				#new edges detected, but I'm out of home. Water running!
 				/opt/usr/bin/telegram-send --config /opt/etc/telegram-send.conf "ALERTA - ALERTA: Fuga de agua en casa"
-				echo "3" > /opt/usr/sbin/waterflow_sensor.strike
-			elif [ $waterflow_sensor_last -eq 1 ] && [ $waterflow_status -eq 0 ]; then
-				#All ok at home. The last water I used is no longer registered
-				echo "0" > /opt/usr/sbin/waterflow_sensor.strike
-			elif [ $waterflow_sensor_last -eq 1 ] && [ $waterflow_status -eq 1 ]; then
-				waterflow_strike=$(awk 'NR==1 {print; exit}' /opt/usr/sbin/waterflow_sensor.strike)
-				waterflow_strike=$((waterflow_strike + 1))
-				echo $waterflow_strike > /opt/usr/sbin/waterflow_sensor.strike
-				if [ $waterflow_strike -ge 3 ]; then
-					#still water running. 
-					/opt/usr/bin/telegram-send --config /opt/etc/telegram-send.conf "ALERTA - ALERTA: Fuga de agua en casa"
-		        	fi
-		        else
-		        	/opt/usr/bin/telegram-send --config /opt/etc/telegram-send.conf "waterflow_sensor sending incorrect data"
 		        fi
-			echo $waterflow_status > /opt/usr/sbin/waterflow_sensor.last
 		fi
 	else
 		/opt/usr/bin/telegram-send --config /opt/etc/telegram-send.conf "waterflow_sensor not sending data"
 	fi
 else
 	#I'm home, so delete flags
-	if [ -e /opt/usr/sbin/waterflow_sensor.strike ]; then
-		rm -rf  /opt/usr/sbin/waterflow_sensor.strike
+	if [ -e /opt/usr/sbin/waterflow_sensor.edges ]; then
+		rm -rf  /opt/usr/sbin/waterflow_sensor.edges
 	fi
-	if [ -e /opt/usr/sbin/waterflow_sensor.last ]; then
-		rm -rf  /opt/usr/sbin/waterflow_sensor.last
-	fi
-
 fi
