95f7806d1abce44afb284bbaaa82a6fa07547619
logon84 <logon84@users.noreply.github.com>
2021-02-23 22:58:43 +0100

    Update watercheck.sh

--- watercheck.sh
+++ watercheck.sh
@@ -12,23 +12,23 @@ export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/lib:/opt/usr/lib"
 amIpresent=$(arp -a | grep $my_smartphone_mac | tr -d ' ')
 if [ -z $amIpresent ]; then
 	#I'm out of home
-	waterflow_status=$(/opt/usr/bin/mosquitto_sub -h $mqtt_broker -u $mqtt_user -P $mqtt_password -t waterflow -q 2 -C 1 -W 5)
-	edges_count=$(echo $waterflow_status | jq -r ".edges")
-	sensor_since=$(echo $waterflow_status | jq -r ".since")
+	/opt/usr/bin/mosquitto_sub -h $mqtt_broker -u $mqtt_user -P $mqtt_password -t waterflow -q 2 -C 1 -W 5 > /opt/usr/sbin/waterflow.topic
+	waterflow_status=$(cat /opt/usr/sbin/waterflow.topic)
+	edges_count=$(cat /opt/usr/sbin/waterflow.topic | jq -r '.edges')
+	sensor_since=$(cat /opt/usr/sbin/waterflow.topic | jq -r '.since')
 	sensor_ip=$(cat /tmp/dhcp.leases | grep waterflow | cut -d' ' -f3)
 	isSensorpresent=$(arp -a | grep $sensor_ip | tr -d ' ')
-	if [ ! -z $waterflow_status ] && [ ! -z $isSensorpresent ]; then
+	if [ ! -z $isSensorpresent ]; then
 		if [ ! -e /opt/usr/sbin/waterflow_sensor.edges ]; then
 			#first water check after leaving home
 			echo $edges_count > /opt/usr/sbin/waterflow_sensor.edges
-			echo "0" > /opt/usr/sbin/waterflow_sensor.notices
 			echo $sensor_since > /opt/usr/sbin/waterflow_sensor.since
 		else
 			#subsequent checks after leaving home
-			edges_count_prev=$(cat "/opt/usr/sbin/waterflow_sensor.edges")
-			sensor_since_prev=$(cat "/opt/usr/sbin/waterflow_sensor.since")
+			edges_count_prev=$(cat /opt/usr/sbin/waterflow_sensor.edges)
+			sensor_since_prev=$(cat /opt/usr/sbin/waterflow_sensor.since)
 			echo $edges_count > /opt/usr/sbin/waterflow_sensor.edges
-			if [ $sensor_since == $sensor_since_prev ]; then
+			if [ "$sensor_since" = "$sensor_since_prev" ]; then
 				if [ $edges_count -ne $edges_count_prev ]; then
 					#new edges detected, but I'm out of home. Water running!
 					/opt/usr/bin/telegram-send --config /opt/etc/telegram-send.conf "ALERTA: Fuga de agua en casa $((($edges_count*0.5/15)-($edges_count_prev*0.5/15))) litros/minuto"
@@ -39,7 +39,11 @@ if [ -z $amIpresent ]; then
 			fi
 		fi
 	else
-		notices=$(cat /opt/usr/sbin/waterflow_sensor.notices)
+		if [ -e /opt/usr/sbin/waterflow_sensor.notices ]; then
+			notices=$(cat /opt/usr/sbin/waterflow_sensor.notices)
+		else
+			notices="0"
+		fi
 		if [ $notices -lt 3 ]; then
 			/opt/usr/bin/telegram-send --config /opt/etc/telegram-send.conf "waterflow_sensor not sending data"
 			notices=$(($notices+1))
@@ -56,5 +60,8 @@ else
 	fi
 	if [ -e /opt/usr/sbin/waterflow_sensor.since ]; then
 		rm -rf  /opt/usr/sbin/waterflow_sensor.since
+	fi
+	if [ -e /opt/usr/sbin/waterflow.topic ]; then
+		rm -rf  /opt/usr/sbin/waterflow.topic
 	fi
 fi
