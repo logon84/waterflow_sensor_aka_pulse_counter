e5c3f3c01a2ace5e47b95a9ae1f55d76ac08fc5d
logon84 <logon84@users.noreply.github.com>
2021-03-06 20:05:12 +0100

    Update watercheck.sh

--- watercheck.sh
+++ watercheck.sh
@@ -12,10 +12,10 @@ export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/opt/lib:/opt/usr/lib"
 amIpresent=$(arp -a | grep $my_smartphone_mac | tr -d ' ')
 if [ -z $amIpresent ]; then
 	#I'm out of home
-	/opt/usr/bin/mosquitto_sub -h $mqtt_broker -u $mqtt_user -P $mqtt_password -t waterflow -q 2 -C 1 -W 5 > /opt/usr/sbin/waterflow.topic
-	waterflow_status=$(cat /opt/usr/sbin/waterflow.topic)
-	edges_count=$(cat /opt/usr/sbin/waterflow.topic | jq -r '.edges')
-	sensor_since=$(cat /opt/usr/sbin/waterflow.topic | jq -r '.since')
+	/opt/usr/bin/mosquitto_sub -h $mqtt_broker -u $mqtt_user -P $mqtt_password -t waterflow -q 2 -C 1 -W 5 > /opt/usr/sbin/waterflow_sensor.topic
+	waterflow_status=$(cat /opt/usr/sbin/waterflow_sensor.topic)
+	edges_count=$(cat /opt/usr/sbin/waterflow_sensor.topic | jq -r '.p_e' | cut -d "_" -f2)
+	sensor_since=$(cat /opt/usr/sbin/waterflow_sensor.topic | jq -r '.since')
 	sensor_ip=$(cat /tmp/dhcp.leases | grep waterflow | cut -d' ' -f3)
 	isSensorpresent=$(arp -a | grep $sensor_ip | tr -d ' ')
 	if [ ! -z $isSensorpresent ]; then
@@ -27,11 +27,28 @@ if [ -z $amIpresent ]; then
 			#subsequent checks after leaving home
 			edges_count_prev=$(cat /opt/usr/sbin/waterflow_sensor.edges)
 			sensor_since_prev=$(cat /opt/usr/sbin/waterflow_sensor.since)
+			sensor_flow=$(cat /opt/usr/sbin/waterflow_sensor.topic | jq -r '.flow')
 			echo $edges_count > /opt/usr/sbin/waterflow_sensor.edges
 			if [ "$sensor_since" = "$sensor_since_prev" ]; then
 				if [ $edges_count -ne $edges_count_prev ]; then
-					#new edges detected, but I'm out of home. Water running!
-					/opt/usr/bin/telegram-send --config /opt/etc/telegram-send.conf "ALERTA: Fuga de agua en casa! ($edges_count_prev, $edges_count) "
+				#new edges detected, but I'm out of home. Water running!
+					if [ $(echo "$sensor_flow > 0.15" | bc -l) -eq 1 ]; then
+						/opt/usr/bin/telegram-send --config /opt/etc/telegram-send.conf "ALERTA: Fuga de agua en casa! ($edges_count_prev, $edges_count) at $sensor_flow L/min "
+					else
+						if [ -e /opt/usr/sbin/waterflow_sensor.strikes ]; then
+							strikes=$(cat /opt/usr/sbin/waterflow_sensor.strikes)
+							strikes=$(($strikes+1))
+						else
+							strikes="1"
+						fi
+						echo $strikes > /opt/usr/sbin/waterflow_sensor.strikes
+						if [ $strikes -gt 2 ]; then
+							/opt/usr/bin/telegram-send --config /opt/etc/telegram-send.conf "ALERTA: Fuga de agua en casa! ($edges_count_prev, $edges_count) at $sensor_flow L/min "
+							
+						fi
+					fi
+				else
+					echo "0" > /opt/usr/sbin/waterflow_sensor.strikes	
 				fi
 			else
 				#power failure while being out, update sensor last boot time
@@ -61,7 +78,18 @@ else
 	if [ -e /opt/usr/sbin/waterflow_sensor.since ]; then
 		rm -rf  /opt/usr/sbin/waterflow_sensor.since
 	fi
-	if [ -e /opt/usr/sbin/waterflow.topic ]; then
-		rm -rf  /opt/usr/sbin/waterflow.topic
+	if [ -e /opt/usr/sbin/waterflow_sensor.topic ]; then
+		rm -rf  /opt/usr/sbin/waterflow_sensor.topic
 	fi
+	if [ -e /opt/usr/sbin/waterflow_sensor.strikes ]; then
+		rm -rf  /opt/usr/sbin/waterflow.strikes
+	fi
+fi
+
+#log status every hour
+curr_min=$(date +'%M')
+if [ "$curr_min" = "00" ]; then
+	waterflow_status_live=$(/opt/usr/bin/mosquitto_sub -h $mqtt_broker -u $mqtt_user -P $mqtt_password -t waterflow -q 2 -C 1 -W 5)
+	curr_date=$(date +'%Y-%m-%d--%H:%M:%S')
+	echo "$curr_date: $waterflow_status_live" >> /opt/var/log/mosquitto.log
 fi
