90773d0521041ae6e8e8610acab5dca274f497dc
logon84 <logon84@users.noreply.github.com>
2021-03-06 20:04:25 +0100

    Update waterflow_sensor.h

--- waterflow_sensor.h
+++ waterflow_sensor.h
@@ -28,4 +28,5 @@ const int PULSE_PIN  = 3;
 const unsigned int DEBOUNCE_MS = 100;
 const unsigned int LED_FLICKER_MS = 200;
 const unsigned int MIN_TIME_BETWEEN_SENDS_MS = 0;
-const float WATERMETER_RESOLUTION_M3 = 0.0001;
+const float RISING_EDGE_LITERS = 0.7;
+const float FALLING_EDGE_LITERS = 0.3;
